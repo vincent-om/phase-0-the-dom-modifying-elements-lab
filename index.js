0d3b17a72724cf293c56b969914e8b8815e4961c
Vincent Makokha <vincent.makokha@student.moringaschool.com>
2022-08-02 14:34:59 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement = document.querySelector('#main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "Vincent is the champion";
